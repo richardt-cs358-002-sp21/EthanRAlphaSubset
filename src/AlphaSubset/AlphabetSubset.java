429f274a991b987cf25ce02c39e57628b1228d1f
ReedEthan <ReedEthan@192.168.1.21>
2021-02-21 18:40:00 -0600

    creating project

---
+++ src/AlphaSubset/AlphabetSubset.java
@@ -0,0 +1,24 @@
+package AlphaSubset;
+/** dubs */
+import java.util.ArrayList;
+
+public class AlphabetSubset {
+
+  public AlphabetSubset(String phrase) {
+    // TODO Auto-generated constructor stub
+  }
+  
+  public ArrayList<Character> findsubset() {
+    ArrayList<Character> subset = new ArrayList<Character>();
+    subset.add('b');
+    subset.add('c');
+    subset.add('d');
+    subset.add('d');
+    subset.add('d');
+    subset.add('f');
+    subset.add('g');
+    subset.add('h');
+    return subset;
+  }
+
+}
---
+++ src/AlphaSubset/AlphabetSubsetTest.java
@@ -0,0 +1,92 @@
+/**
+ * test1
+ */
+package AlphaSubset;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * @author ReedEthan
+ *
+ */
+class AlphabetSubsetTest {
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @BeforeAll
+  static void setUpBeforeClass() throws Exception {
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @AfterAll
+  static void tearDownAfterClass() throws Exception {
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @BeforeEach
+  void setUp() throws Exception {
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @AfterEach
+  void tearDown() throws Exception {
+  }
+  
+  @Test
+  void constructorTest() {
+    AlphabetSubset test1 = new AlphabetSubset("oh i get it");
+    Assertions.assertNotNull(test1);
+  }
+  
+  @Test
+  void findSimpleSubset() {
+    AlphabetSubset test1 = new AlphabetSubset("abcde");
+    ArrayList<Character> subset = test1.findsubset();
+    
+    ArrayList<Character> expected = new ArrayList<Character>();
+    expected.add('a');
+    expected.add('b');
+    expected.add('c');
+    expected.add('d');
+    expected.add('e');
+    
+    Assertions.assertArrayEquals(expected.toArray(), subset.toArray());
+  }
+  
+  @Test
+  void findComplexSubset() {
+    AlphabetSubset test1 = new AlphabetSubset("bcdddfgh");
+    ArrayList<Character> subset = test1.findsubset();
+    
+    ArrayList<Character> expected = new ArrayList<Character>();
+    expected.add('b');
+    expected.add('c');
+    expected.add('d');
+    expected.add('d');
+    expected.add('d');
+    expected.add('f');
+    expected.add('g');
+    expected.add('h');
+    
+    Assertions.assertArrayEquals(expected.toArray(), subset.toArray());
+  }
+
+  @Test
+  void assertEquals(boolean expected, boolean actual) {
+  }
+}
